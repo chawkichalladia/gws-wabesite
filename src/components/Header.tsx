640cf1b202a52e684f04d48b42102fa0b6d8cc82
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2023-09-24 00:06:57 +0100

    Apply font colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div>
         <Image src={Logo} alt="Galactech Web Solutions logo" className="w-20 md:w-40 h-12 md:h-24" />
       </div>
-      <button className="bg-white px-3 md:px-6 py-2 md:py-4 text-md md:text-lg rounded-full flex align-items-center gap-2.5 self-center">
+      <button className="bg-white text-black px-3 md:px-6 py-2 md:py-4 text-md md:text-lg rounded-full flex align-items-center gap-2.5 self-center">
         <div className={poppins.className}>Let's talk</div>
         <div className="flex align-items-center">
           <Image src={ArrowCircleRight} alt="Round right caret" />
