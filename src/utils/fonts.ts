75763f4bdd0b1162efa4124bb53bd0246bc45b56
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2023-09-23 23:11:26 +0100

    Add needed fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,4 +1,13 @@
+import { Open_Sans, Poppins } from 'next/font/google';
+import local from 'next/font/local';
 
-import { Open_Sans } from "next/font/google";
-
-export const open_Sans = Open_Sans({ subsets: ["latin"] });
+export const open_Sans = Open_Sans({ subsets: ['latin'] });
+export const poppins = Poppins({ weight: '400', subsets: ['latin'] });
+export const orion_pax = local({
+  src: [
+    { path: '../../public/orion-pax/orion_pax.otf', weight: '400', style: 'normal' },
+    { path: '../../public/orion-pax/orion_pax_italic.otf', weight: '400', style: 'italic' },
+    { path: '../../public/orion-pax/orion_pax_bold.otf', weight: '700', style: 'normal' },
+    { path: '../../public/orion-pax/orion_pax_bold_italic.otf', weight: '700', style: 'italic' },
+  ],
+});
