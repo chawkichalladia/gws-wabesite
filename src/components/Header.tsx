282812d9041600c5f09bde32c45543738a68845a
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2023-09-23 23:14:37 +0100

    Apply font to header button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,18 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import ArrowCircleRight from "../../public/arrow-circle-right.svg";
-import Logo from "../../public/logo.png";
+import ArrowCircleRight from '../../public/arrow-circle-right.svg';
+import Logo from '../../public/logo.png';
+
+import { poppins } from '@/utils/fonts';
 
 const Header = () => {
   return (
     <div className="flex justify-between align-items-center absolute container max-w-[90%]">
       <div>
-        <Image
-          src={Logo}
-          alt="Galactech Web Solutions logo"
-          className="w-20 md:w-40 h-12 md:h-24"
-        />
+        <Image src={Logo} alt="Galactech Web Solutions logo" className="w-20 md:w-40 h-12 md:h-24" />
       </div>
       <button className="bg-white px-3 md:px-6 py-2 md:py-4 text-md md:text-lg rounded-full flex align-items-center gap-2.5 self-center">
-        <div>Let's talk</div>
+        <div className={poppins.className}>Let's talk</div>
         <div className="flex align-items-center">
           <Image src={ArrowCircleRight} alt="Round right caret" />
         </div>
