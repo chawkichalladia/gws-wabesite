81c5f8a4df925e561cc0113a3bcd8eeba80fd9a4
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2025-01-27 23:32:45 +0100

    Add animation for the women in the contact popup drawing

--- src/components/images/ContactUsImage.tsx
+++ src/components/images/ContactUsImage.tsx
@@ -1,18 +1,24 @@
+import clsx from 'clsx';
 import Image from 'next/image';
+import { useContext } from 'react';
 
 import GirlWithLaptopAndPopupImage from '../../../public/contact_us/girl_with_laptop_and_popup.png';
 import GoldStarImage from '../../../public/contact_us/gold_star.png';
 import GreyStarImage from '../../../public/contact_us/grey_star.png';
 import MessageImage from '../../../public/contact_us/message.png';
 
+import { GlobalContext } from '@/context';
+
 export const ContactUsImage = () => {
+  const { open } = useContext(GlobalContext);
+
   return (
     <div className="w-full max-w-[402px] relative aspect-[2016/2254]">
-      <Image src={GirlWithLaptopAndPopupImage} alt="A girl carrying a laptop and leaning on a popup" className="absolute w-full h-full left-0 top-0" />
-      <Image src={GoldStarImage} alt="A gold star from a review" className="absolute w-[7.788%] h-[6.655%] left-[49.107%] top-[19.343%] z-10" />
-      <Image src={GoldStarImage} alt="A gold star from a review" className="absolute w-[7.788%] h-[6.655%] left-[58.879%] top-[19.343%] z-10" />
-      <Image src={GoldStarImage} alt="A gold star from a review" className="absolute w-[7.788%] h-[6.655%] left-[68.651%] top-[19.343%] z-10" />
-      <Image src={GoldStarImage} alt="A gold star from a review" className="absolute w-[7.788%] h-[6.655%] left-[78.423%] top-[19.343%] z-10" />
+      <Image
+        src={GirlWithLaptopAndPopupImage}
+        alt="A girl carrying a laptop and leaning on a popup"
+        className={clsx('absolute w-0 h-0 left-0 bottom-0', { ['contact-image-person']: open })}
+      />
       <Image
         src={GreyStarImage}
         alt="A grey star from a review"
