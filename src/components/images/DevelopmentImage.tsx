eab76b164c0fb4a10e6c50a22ef220bbe895d5b6
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2025-01-28 17:24:22 +0100

    Add animations for development section image

--- src/components/images/DevelopmentImage.tsx
+++ src/components/images/DevelopmentImage.tsx
@@ -9,22 +9,30 @@ import XImage from '../../../public/development/x.png';
 export const DevelopmentImage = () => {
   return (
     <div className="w-full max-w-[462px] relative aspect-[462/438]">
-      <Image src={XImage} alt="X icon image" className="absolute w-[4.76%] h-[5.25%] left-[49.78%] top-[1.83%]" />
-      <Image src={LeftBoxImage} alt="TV image" className="absolute w-[40.91%] h-[34.25%] left-0 top-[10.73%] z-10" />
+      <Image
+        src={XImage}
+        alt="X icon image"
+        className="absolute w-0 h-[5.25%] left-[49.78%] top-[1.83%] development-x"
+      />
+      <Image
+        src={LeftBoxImage}
+        alt="TV image"
+        className="absolute w-[40.91%] h-[34.25%] left-0 top-[10.73%] z-10 opacity-0 development-left-box"
+      />
       <Image
         src={SittingManImage}
         alt="A man sitting with a laptop on his lap"
-        className="absolute w-[49.35%] h-[81.51%] left-[19.48%] top-[13.39%]"
+        className="absolute w-[49.35%] h-[81.51%] left-[19.48%] top-[13.39%] opacity-0 development-person"
       />
       <Image
         src={RightBoxImage}
         alt="Abstract image of a TV"
-        className="absolute w-[29.87%] h-[21.46%] right-0 top-[39.04%]"
+        className="absolute w-[29.87%] h-[21.46%] right-0 top-[39.04%] opacity-0 development-right-box"
       />
       <Image
         src={CircleImage}
         alt="Circle icon image"
-        className="absolute w-[3.68%] h-[3.88%] left-[83.77%] top-[64.61%]"
+        className="absolute w-[3.68%] h-[3.88%] left-[83.77%] top-[64.61%] opacity-0 development-circle"
       />
     </div>
   );
