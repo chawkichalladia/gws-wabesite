a853afb997ec7d8c63adf9784a2cf513149b48a6
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2025-02-08 20:16:56 +0100

    WIP: Fix lintstaged configs

--- apps/website/.lintstagedrc.js
+++ apps/website/.lintstagedrc.js
@@ -1,12 +1,11 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) => {
-  console.log(filenames);
   return `yarn workspace website run lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 };
 
 module.exports = {
-  '*.{ts,tsx}': [buildEslintCommand],
+  '*.{ts,tsx}': ["yarn workspace website run lint --"],
 };
--- apps/website/eslint.config.mjs
+++ apps/website/eslint.config.mjs
@@ -1,4 +1,4 @@
 import { nextConfigs } from '@packages/eslint-config/next';
 
 /** @type {import("eslint").Linter.Config} */
-export default [...nextConfigs, {ignores: ["*.js"]}];
+export default [...nextConfigs, {ignores: []}];
