302ba0696ecdc1fad228a1e10bfe91bcf7cf5928
Chawki Ben Challadia <chawki.challadia@galactechwebsolutions.com>
2025-02-08 22:04:23 +0100

    Add formatting scripts

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-    '**/*.{js,jsx,ts,tsx,md,json}': ["yarn format --"],
-  };
--- apps/website/.lintstagedrc.js
+++ apps/website/.lintstagedrc.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 module.exports = {
-  '*.{ts,tsx}': ["yarn workspace website run lint"],
+  '*.{ts,tsx}': ["yarn workspace website run lint", "yarn format"],
 };
